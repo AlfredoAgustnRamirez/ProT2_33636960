c625f1acfa20950899c256e9b6219d57a914fc65
Agustinn35 <ramirezalfredoagustin80@mail.com>
2024-06-21 17:17:43 -0300

    first commit

---
+++ app/Views/principal.php
@@ -0,0 +1,106 @@
+<body>
+
+  <section>
+    <div class="container-fluid mt-3">
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="public/assets/img/celulares.jpg" class="d-block w-100 img-fluid" alt="imagen 1" style="height: 350px; max-height: 100%;">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Servicio técnico especializado</h5>
+              <p>Nos enfocamos en brindar el mejor servicio técnico en la región.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="public/assets/img/celulares2.jpg" class="d-block w-100 img-fluid" alt="imagen 2" style="height: 350px; max-height: 100%;">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Venta de accesorios premium</h5>
+              <p>Ofrecemos una amplia gama de accesorios premium para celulares y computadoras.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="public/assets/img/celulares3.jpg" class="d-block w-100 img-fluid" alt="imagen 3" style="height: 350px; max-height: 100%;">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Reparación rápida y eficiente</h5>
+              <p>Garantizamos reparaciones rápidas y eficientes para todos los tipos de dispositivos.</p>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden"></span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden"></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="py-3">
+    <div class="contenedor px-4 px-lg-5 mt-1">
+      <div class="jumbo">
+        <div class="container">
+          <h1 class="jumbo-title text-center">Productos Destacados</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container mt-1">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img src="public/assets/uploads/cargador.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Cargadores Aitech</h5>
+              <p class="card-text">Super oferta de cargadores Aitech.</p>
+              <a href="#!" class="btn btn-primary">Ver más</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img src="public/assets/uploads/fundas.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Fundas de celulares</h5>
+              <p class="card-text">50% Off en todos los modelos de celulares.</p>
+              <a href="#!" class="btn btn-primary">Ver más</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img src="public/assets/uploads/memorias.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Memorias Gammers</h5>
+              <p class="card-text">30% Off en memorias para tu PC gammers.</p>
+              <a href="#!" class="btn btn-primary">Ver más</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 mb-4">
+          <div class="card">
+            <img src="public/assets/uploads/teclado.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Teclados Gammers</h5>
+              <p class="card-text">20% Off para todos los teclados gammers.</p>
+              <a href="#!" class="btn btn-primary">Ver más</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</body>
---
+++ app/Views/registrarse.php
@@ -0,0 +1,42 @@
+<div class="jumbo mt-3">
+  <div class="container">
+    <h1 class="jumbo-title text-center">Registrarse</h1>
+  </div>
+</div>
+<div class="container mt-1 mb-0 mt-4">
+  <div class="card" style="width: 50%;">
+    <form>
+      <div class="card-body">
+        <div class="mb-2">
+          <label for="exampleFormControlInput1" class="form-label">Nombre</label>
+          <input name="nombre" type="text" class="form-control" placeholder="Nombre">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Apellido</label>
+          <input type="text" name="apellido" class="form-control" placeholder="Apellido">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Email</label>
+          <input name="email" type="email" class="form-control" placeholder="correo@algo.com">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Usuario</label>
+          <input type="text" name="usuario" class="form-control" placeholder="Usuario">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Contraseña</label>
+          <input name="password" type="password" class="form-control" placeholder="Contraseña">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Confirmar Contraseña</label>
+          <input name="passconf" type="password" class="form-control" placeholder="Repita su contraseña">
+        </div>
+        <div class="custom-control custom-checkbox mb-3">
+          <input id="customCheck1" type="checkbox" checked class="custom-control-input">
+          <label for="customCheck1" class="custom-control-label">Recordar contraseña</label>
+        </div>
+        <button type="button" class="btn btn-primary btn-block">Guardar</button>
+      </div>
+    </form>
+  </div>
+</div>
